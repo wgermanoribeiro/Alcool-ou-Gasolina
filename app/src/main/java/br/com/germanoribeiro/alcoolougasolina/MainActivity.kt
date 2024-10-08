1d1e4c3d60b278f46712e8eb2b9ab46cbc0d980d
wgermanoribeiro <w.germanoribeiro@gmail.com>
2024-10-08 20:03:15 -0300

    Inicio do projeto

---
+++ app/src/main/java/br/com/germanoribeiro/alcoolougasolina/MainActivity.kt
@@ -0,0 +1,48 @@
+package br.com.germanoribeiro.alcoolougasolina
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.germanoribeiro.alcoolougasolina.ui.theme.AlcoolOuGasolinaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AlcoolOuGasolinaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+
+    Column {
+        Text(text = "Álcool ou Gasolina")
+        Text(text = "Gasolina")
+        TextField(value = "2.00", onValueChange = {} )
+        TextField(value = "2.00", onValueChange = {} )
+    }
+
+}
