badd59307f2dae893c16954922aa3d9a69726bf9
wgermanoribeiro <w.germanoribeiro@gmail.com>
2024-10-18 22:00:46 -0300

    - Inicio da estilização das telas adicionais, inicio pela tela EntendaSobreOCalculoScreen.kt

--- app/src/main/java/br/com/germanoribeiro/alcoolougasolina/EntendaSobreOCalculoScreen.kt
+++ app/src/main/java/br/com/germanoribeiro/alcoolougasolina/EntendaSobreOCalculoScreen.kt
@@ -1,15 +1,44 @@
 package br.com.germanoribeiro.alcoolougasolina
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun EntendaSobreOCalculoScreen() {
-	Column(modifier = Modifier.padding(16.dp)) {Text("Nesta tela, você encontrará uma explicação detalhada sobre como o cálculo do aplicativo é realizado.")
-		// Adicione mais texto explicativo aqui
+	Column(
+		Modifier
+			.background(color = Color(222, 222, 222, 255))
+			.fillMaxSize(),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally
+	){
+		Column (
+			verticalArrangement = Arrangement.spacedBy(16.dp),
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			Text(
+				text = "Álcool ou Gasolina?",
+				style = TextStyle(
+					color = Color.DarkGray,
+					fontSize = 32.sp,
+					fontWeight = FontWeight.Bold
+				)
+			)
+			
+			Text("Nesta tela, você encontrará uma explicação detalhada sobre como o cálculo do aplicativo é realizado.")
+			
+		}
+		
 	}
 }
