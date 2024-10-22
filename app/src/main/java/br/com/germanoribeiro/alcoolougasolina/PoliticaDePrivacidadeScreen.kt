e5033748e00d88c1cfeab77cc74fcf0ca626ba39
wgermanoribeiro <w.germanoribeiro@gmail.com>
2024-10-21 21:45:09 -0300

    - Alterado os icones para retorno para a tela principal por recomendação do Android Studio

--- app/src/main/java/br/com/germanoribeiro/alcoolougasolina/PoliticaDePrivacidadeScreen.kt
+++ app/src/main/java/br/com/germanoribeiro/alcoolougasolina/PoliticaDePrivacidadeScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -36,7 +36,7 @@ fun PoliticaDePrivacidadeScreen(navController: NavController) {
 				.padding(8.dp) // Adiciona espaçamento
 		) {
 			Icon(
-				imageVector = Icons.Filled.ArrowBack,
+				imageVector = Icons.AutoMirrored.Filled.ArrowBack,
 				contentDescription = "Voltar"
 			)
 		}
